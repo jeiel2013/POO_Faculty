3ab9e3295e575ad1bd8a5bbcc1b66535910c9bf0
jeiel2013 <contatojeiel2013@gmail.com>
2024-05-31 15:01:15 -0300

    aula 29/05

---
+++ ProjetoCadastro/src/main/java/Cadastro.java
@@ -0,0 +1,85 @@
+public class Cadastro {
+    private String Matricula;
+    private String Nome;
+    private String Cpf;
+    private String Celular;
+    private String Email;
+    private String Status;
+    private String Endereco;
+    private String DataNascimento;
+    private double Salario;
+
+    public String getMatricula() {
+        return Matricula;
+    }
+
+    public void setMatricula(String Matricula) {
+        this.Matricula = Matricula;
+    }
+
+    public String getNome() {
+        return Nome;
+    }
+
+    public void setNome(String Nome) {
+        this.Nome = Nome;
+    }
+
+    public String getCpf() {
+        return Cpf;
+    }
+
+    public void setCpf(String Cpf) {
+        this.Cpf = Cpf;
+    }
+
+    public String getCelular() {
+        return Celular;
+    }
+
+    public void setCelular(String Celular) {
+        this.Celular = Celular;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String Email) {
+        this.Email = Email;
+    }
+
+    public String getStatus() {
+        return Status;
+    }
+
+    public void setStatus(String Status) {
+        this.Status = Status;
+    }
+
+    public String getEndereco() {
+        return Endereco;
+    }
+
+    public void setEndereco(String Endereco) {
+        this.Endereco = Endereco;
+    }
+
+    public String getDataNascimento() {
+        return DataNascimento;
+    }
+
+    public void setDataNascimento(String DataNascimento) {
+        this.DataNascimento = DataNascimento;
+    }
+
+    public double getSalario() {
+        return Salario;
+    }
+
+    public void setSalario(double Salario) {
+        this.Salario = Salario;
+    }
+    
+    
+}
